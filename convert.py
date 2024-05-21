f949008eb7406dda79d9ca6c451ac4c16b37c67e
mashashelegeda <masha0shelegeda@gmail.com>
2024-05-21 22:03:00 +0300

    add xml save func

--- convert.py
+++ convert.py
@@ -11,6 +11,25 @@ def main():
 
     result = ""
     files_format = args.input_file.split('.')[-1].lower()
+    if files_format == 'json':
+        result = json_load(args.input_file)
+    elif files_format == 'yaml' or files_format == 'yml':
+        result = yaml_load(args.input_file)
+    elif files_format == 'xml':
+        result = xml_load(args.input_file)
+    else:
+        print("Available formats: json, yml(yaml), xml")
+        return
+
+    output_format = args.output_file.split(".")[-1].lower()
+    if output_format == 'json':
+        json_save(result, args.output_file)
+    elif output_format in ['yml', 'yaml']:
+        yaml_save(result, args.output_file)
+    elif output_format == 'xml':
+        xml_save(result, args.output_file)
+    else:
+        print('Available formats: json, yml(yaml), xml')
 
 
 def json_load(file_path):
@@ -32,6 +51,11 @@ def yaml_save(result, file_path):
 def xml_load(file_path):
     with open(file_path, 'r') as file:
         return xmltodict.parse(file.read())
+        
+def xml_save(result, file_path):
+    with open(file_path, "w") as file:
+        xmltodict.unparse(result, output=file, pretty=True)
+
 
       
 if __name__ == "__main__":
