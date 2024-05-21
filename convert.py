d045565d51afe78c8a46928dd420f8e174128967
mashashelegeda <masha0shelegeda@gmail.com>
2024-05-21 21:38:18 +0300

    add yaml load func

--- convert.py
+++ convert.py
@@ -20,6 +20,10 @@ def json_load(file_path):
 def json_save(result, file_path):
     with open(file_path, 'w')as file:
         return json.dump(result, file)
+        
+def yaml_load(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
 
       
 if __name__ == "__main__":
